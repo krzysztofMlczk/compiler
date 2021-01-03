0842802c6125c8880ea3c4b25e2499d46eef83c4
Krzysztof Malczak <gajderstudio@gmail.com>
2021-01-03 23:57:08 +0100

    CondLeq getCode() method implementation

--- compiler/blocks/conditions/cCondLeq.cpp
+++ compiler/blocks/conditions/cCondLeq.cpp
@@ -4,6 +4,18 @@ CondLeq::CondLeq(Value* val1, Value* val2):Condition(val1, val2) {
     
 }
 
-string CondLeq::getCode() {
-    return "";
+vector<string> CondLeq::getCode(SymbolTable* symbolTable) {
+    vector<string> code;
+    this->val1->outcome_reg = this->outcome_reg;
+    code = this->val1->getCode(symbolTable);
+
+    this->val2->outcome_reg = this->clobbers.at(0);
+    vector<string> code1 = this->val2->getCode(symbolTable);
+
+    // append code1 to code
+    code.insert(code.end(), code1.begin(), code1.end());
+
+    code.push_back("SUB " + this->outcome_reg + " " + this->clobbers.at(0));
+
+    return code;
 }
--- compiler/blocks/conditions/cCondLeq.hpp
+++ compiler/blocks/conditions/cCondLeq.hpp
@@ -1,17 +1,15 @@
 #ifndef CCONDLEQ_HPP
 #define CCONDLEQ_HPP
 #include "../../interfaces/iCondition.hpp"
-#include <string>
 
 /*
 *   Condition "<=" CLASS
 */
 class CondLeq: public Condition {
     public:
+        const ull clobber_counter = 1;
         CondLeq(Value* v1, Value* v2);
-        virtual string getCode();
-    protected:
-    private:
+        virtual vector<string> getCode(SymbolTable* symbolTable);
 };
 
 #endif

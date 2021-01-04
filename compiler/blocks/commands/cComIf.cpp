b0b4351bf915581d5663badccda9b4a1f8648682
Krzysztof Malczak <gajderstudio@gmail.com>
2021-01-04 14:29:19 +0100

    ComIf getCode() method implementation

--- compiler/blocks/commands/cComIf.cpp
+++ compiler/blocks/commands/cComIf.cpp
@@ -5,6 +5,22 @@ ComIf::ComIf(Condition* cond, vector<Command*>* cmds) {
     this->commands = cmds;
 }
 
-string ComIf::getCode() {
-    return "";
+vector<string> ComIf::getCode(SymbolTable* symbolTable) {
+    vector<string> code;
+    vector<string> occupied_registers{"a"};
+
+    // assign out_reg for condition
+    this->condition->outcome_reg = "a";
+    // every condition requires clobbers, then assign them
+    this->condition->clobbers = this->getClobbers(&occupied_registers, this->condition->clobber_counter);
+    code = this->condition->getCode(symbolTable);
+
+    int jump = this->commands->size() + 1;
+
+    code.push_back("JZERO a 1");
+    code.push_back("JUMP " + to_string(jump));
+    // append cmds to perform when condition output is true
+    code.insert(code.end(), this->commands->begin(), this->commands->end());
+
+    return code;
 }

d890b75be2bf7d5913d630d981b025bd3351b1b2
Krzysztof Malczak <gajderstudio@gmail.com>
2021-01-04 15:49:37 +0100

    ComWhile getCode() method implementation

--- compiler/blocks/commands/cComWhile.cpp
+++ compiler/blocks/commands/cComWhile.cpp
@@ -15,12 +15,18 @@ vector<string> ComWhile::getCode(SymbolTable* symbolTable) {
     this->condition->clobbers = this->getClobbers(&occupied_registers, this->condition->clobber_counter);
     code = this->condition->getCode(symbolTable);
 
-    int jump = this->commands->size() + 1;
+    // before each loop iteration condition has to be checked
+    int cond_check_len = code.size();
+    int commands_len = this->commands->size();
 
+    // condition checking
     code.push_back("JZERO a 2"); // go to commands
-    code.push_back("JUMP " + to_string(jump));
+    code.push_back("JUMP " + to_string(commands_len + 2)); // go to end
     // append commands to code
     code.insert(code.end(), this->commands->begin(), this->commands->end());
+    
+    // jump to begining of condition checking
+    code.push_back("JUMP -" + to_string(cond_check_len + 2 + commands_len));
 
     return code;
 }
